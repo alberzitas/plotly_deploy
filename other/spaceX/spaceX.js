e33f42a1291f0faaa4b01803923e95c75ae76fb8
Albert Zeng <107147118+alberzitas@users.noreply.github.com>
2022-09-05 16:20:07 -0700

    Add files via upload

---
+++ other/spaceX/spaceX.js
@@ -0,0 +1,9 @@
+
+d3.json("samples.json").then(function(data){
+    console.log("hello");
+});
+
+// when you get it to work...
+d3.json("samples.json").then(function(data){
+    console.log(data);
+});
